138f959b9d0702482ae373997be52c072dc8be73
fwani <seungfwani@gmail.com>
2022-01-06 10:06:30 +0900

    Add OCR function using tesseract.js
    Authored-by: fwani <seungfwani@gmail.com>
    Signed-off-by: fwani <seungfwani@gmail.com>

--- front/src/scripts/image-parser.js
+++ front/src/scripts/image-parser.js
@@ -1,16 +1,5 @@
 import Tesseract from "tesseract.js";
 
-// async function get_text_from_image(image) {
-//     const { data: { text } } = await Tesseract.recognize(image, 'eng', {
-//         logger: m => console.log(m),
-//     });
-//     console.log(text);
-//     return text;
-// }
-
-// var a = await get_text_from_image('https://tesseract.projectnaptha.com/img/eng_bw.png')
-//
-// console.log(a)
 
 let get_text_from_image = async (image) => {
     const {data: {text}} = await Tesseract.recognize(image, 'eng', {
